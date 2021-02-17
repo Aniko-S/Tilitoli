c077715c6aeb65cd5ee346bf2a9ce5936a5fbdc8
Aniko <s.aniko9424@gmail.com>
2021-02-17 22:19:26 +0100

    Console.log 'change' if we can change the clicked field with the empty field

--- src/Table.js
+++ src/Table.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Button from './Button';
 import InitializeMap from './InitializeMap';
 
-const searchEmptyField = (field) => {
-  for (let i = 0; i < field.length; i++) {
-    for (let j = 0; j < field.length; j++) {
-      if (field[i][j].text === '') {
-        console.log(i, ', ', j);
-        return { i, j };
-      }
+const step = (map, clickedField) => {
+  const emptyField = searchEmptyField(map);
+  if (canStep(clickedField, emptyField)) {
+    changeFields(clickedField, emptyField);
+  }
+}
+
 const searchEmptyField = (map) => {
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map.length; j++) {
@@ -19,26 +19,40 @@ const searchEmptyField = (map) => {
   }
 }
 
+const canStep = (clickedField, emptyField) => {
+  if (Math.abs(clickedField.x - emptyField.x) === 1 && clickedField.y === emptyField.y) {
+    return true;
+  }
+  if (Math.abs(clickedField.y - emptyField.y) === 1 && clickedField.x === emptyField.x) {
+    return true;
+  }
+  return false;
+}
+
+const changeFields = (fieldA, fieldB) => {
+  console.log('change');
+}
+
 function Table() {
   const map = InitializeMap();
-  
+
   return (
     <div className='container'>
       <table>
         <tr>
-          <td><Button onClick={() => searchEmptyField(map)}>{map[0][0].text}</Button></td>
-          <td><Button onClick={() => searchEmptyField(map)}>{map[0][1].text}</Button></td>
-          <td><Button onClick={() => searchEmptyField(map)}>{map[0][2].text}</Button></td>
+          <td><Button onClick={() => step(map, map[0][0])}>{map[0][0].text}</Button></td>
+          <td><Button onClick={() => step(map, map[0][1])}>{map[0][1].text}</Button></td>
+          <td><Button onClick={() => step(map, map[0][2])}>{map[0][2].text}</Button></td>
         </tr>
         <tr>
-          <td><Button onClick={() => searchEmptyField(map)}>{map[1][0].text}</Button></td>
-          <td><Button onClick={() => searchEmptyField(map)}>{map[1][1].text}</Button></td>
-          <td><Button onClick={() => searchEmptyField(map)}>{map[1][2].text}</Button></td>
+          <td><Button onClick={() => step(map, map[1][0])}>{map[1][0].text}</Button></td>
+          <td><Button onClick={() => step(map, map[1][1])}>{map[1][1].text}</Button></td>
+          <td><Button onClick={() => step(map, map[1][2])}>{map[1][2].text}</Button></td>
         </tr>
         <tr>
-          <td><Button onClick={() => searchEmptyField(map)}>{map[2][0].text}</Button></td>
-          <td><Button onClick={() => searchEmptyField(map)}>{map[2][1].text}</Button></td>
-          <td><Button onClick={() => searchEmptyField(map)}>{map[2][2].text}</Button></td>
+          <td><Button onClick={() => step(map, map[2][0])}>{map[2][0].text}</Button></td>
+          <td><Button onClick={() => step(map, map[2][1])}>{map[2][1].text}</Button></td>
+          <td><Button onClick={() => step(map, map[2][2])}>{map[2][2].text}</Button></td>
         </tr>
       </table>
     </div>
