e4fe88aa47a88c3dcc7b9e8190d39cc90c6361fb
Aniko <s.aniko9424@gmail.com>
2021-02-23 19:57:52 +0100

    Check the puzzle is solvable and change the last two elements if it is not solvable

--- src/InitializeMap.js
+++ src/InitializeMap.js
@@ -12,6 +12,30 @@ const shuffleNumberList = () => {
     const j = Math.floor(Math.random() * (i + 1));
     [numberList[i], numberList[j]] = [numberList[j], numberList[i]];
   }
+  if (!isValid()) {
+    console.log("change");
+    changeNotEmptyLastTwo();
+  }
+};
+
+const isValid = () => {
+  let count = 0;
+  for (let i = 0; i < numberList.length - 2; i++) {
+    for (let j = i + 1; j < numberList.length - 1; j++) {
+      if (numberList[i] > numberList[j]) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+  return count % 2 === 0;
+};
+
+const changeNotEmptyLastTwo = () => {
+  const lastNotEmptyIndex = numberList.length - 2;
+  const lastNotEmptyElement = numberList[lastNotEmptyIndex];
+  numberList[lastNotEmptyIndex] = numberList[lastNotEmptyIndex - 1];
+  numberList[lastNotEmptyIndex - 1] = lastNotEmptyElement;
 };
 
 const fillMap = () => {
