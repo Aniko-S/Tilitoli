f8c83459311820851209a964dbc60d03aee33820
Aniko <s.aniko9424@gmail.com>
2021-02-23 20:33:40 +0100

    Import baseMap

--- src/Table.js
+++ src/Table.js
@@ -1,11 +1,8 @@
-import React, { useState } from 'react';
-import Button from './Button';
-import InitializeMap from './InitializeMap';
-
-
+import React, { useState } from "react";
+import Button from "./Button";
+import baseMap from "./InitializeMap";
 
 function Table() {
-  const baseMap = InitializeMap();
   const [map, setMap] = useState(baseMap);
 
   const step = (clickedField) => {
@@ -13,27 +10,33 @@ function Table() {
     if (canStep(clickedField, emptyField)) {
       changeFields(clickedField, emptyField);
     }
-  }
+  };
 
   const searchEmptyField = () => {
     for (let i = 0; i < map.length; i++) {
       for (let j = 0; j < map.length; j++) {
-        if (map[i][j].text === '') {
+        if (map[i][j].text === "") {
           return map[i][j];
         }
       }
     }
-  }
+  };
 
   const canStep = (clickedField, emptyField) => {
-    if (Math.abs(clickedField.x - emptyField.x) === 1 && clickedField.y === emptyField.y) {
+    if (
+      Math.abs(clickedField.x - emptyField.x) === 1 &&
+      clickedField.y === emptyField.y
+    ) {
       return true;
     }
-    if (Math.abs(clickedField.y - emptyField.y) === 1 && clickedField.x === emptyField.x) {
+    if (
+      Math.abs(clickedField.y - emptyField.y) === 1 &&
+      clickedField.x === emptyField.x
+    ) {
       return true;
     }
     return false;
-  }
+  };
 
   const changeFields = (fieldA, fieldB) => {
     const tempMap = [[...map[0]], [...map[1]], [...map[2]]];
@@ -41,25 +44,43 @@ function Table() {
     tempMap[fieldA.x][fieldA.y].text = fieldB.text;
     tempMap[fieldB.x][fieldB.y].text = tempText;
     setMap(() => tempMap);
-  }
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <table>
         <tr>
-          <td><Button onClick={() => step(map[0][0])}>{map[0][0].text}</Button></td>
-          <td><Button onClick={() => step(map[0][1])}>{map[0][1].text}</Button></td>
-          <td><Button onClick={() => step(map[0][2])}>{map[0][2].text}</Button></td>
+          <td>
+            <Button onClick={() => step(map[0][0])}>{map[0][0].text}</Button>
+          </td>
+          <td>
+            <Button onClick={() => step(map[0][1])}>{map[0][1].text}</Button>
+          </td>
+          <td>
+            <Button onClick={() => step(map[0][2])}>{map[0][2].text}</Button>
+          </td>
         </tr>
         <tr>
-          <td><Button onClick={() => step(map[1][0])}>{map[1][0].text}</Button></td>
-          <td><Button onClick={() => step(map[1][1])}>{map[1][1].text}</Button></td>
-          <td><Button onClick={() => step(map[1][2])}>{map[1][2].text}</Button></td>
+          <td>
+            <Button onClick={() => step(map[1][0])}>{map[1][0].text}</Button>
+          </td>
+          <td>
+            <Button onClick={() => step(map[1][1])}>{map[1][1].text}</Button>
+          </td>
+          <td>
+            <Button onClick={() => step(map[1][2])}>{map[1][2].text}</Button>
+          </td>
         </tr>
         <tr>
-          <td><Button onClick={() => step(map[2][0])}>{map[2][0].text}</Button></td>
-          <td><Button onClick={() => step(map[2][1])}>{map[2][1].text}</Button></td>
-          <td><Button onClick={() => step(map[2][2])}>{map[2][2].text}</Button></td>
+          <td>
+            <Button onClick={() => step(map[2][0])}>{map[2][0].text}</Button>
+          </td>
+          <td>
+            <Button onClick={() => step(map[2][1])}>{map[2][1].text}</Button>
+          </td>
+          <td>
+            <Button onClick={() => step(map[2][2])}>{map[2][2].text}</Button>
+          </td>
         </tr>
       </table>
     </div>
