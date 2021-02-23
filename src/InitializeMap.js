91a88b4b3e97fd6a02cb67af5110376a8c7b6c79
Aniko <s.aniko9424@gmail.com>
2021-02-23 20:16:59 +0100

    Move out fill numberList to initialValues.js file because InitializeMap function ran for every render

--- src/InitializeMap.js
+++ src/InitializeMap.js
@@ -1,67 +1,29 @@
+import initialValues from "./initialValues";
+
 const baseMap = [
   ["", "", ""],
   ["", "", ""],
   ["", "", ""],
 ];
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", ""];
 
-const numberList = [...numbers];
+const numberList = initialValues();
 
-const shuffleNumberList = () => {
-  for (let i = numberList.length - 2; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [numberList[i], numberList[j]] = [numberList[j], numberList[i]];
-  }
-  if (!isValid()) {
-    console.log("change");
-    changeNotEmptyLastTwo();
-  }
-};
+const objectList = [
+  { x: 0, y: 0, text: numberList[0] },
+  { x: 0, y: 1, text: numberList[1] },
+  { x: 0, y: 2, text: numberList[2] },
+  { x: 1, y: 0, text: numberList[3] },
+  { x: 1, y: 1, text: numberList[4] },
+  { x: 1, y: 2, text: numberList[5] },
+  { x: 2, y: 0, text: numberList[6] },
+  { x: 2, y: 1, text: numberList[7] },
+  { x: 2, y: 2, text: numberList[8] },
+];
 
-const isValid = () => {
-  let count = 0;
-  for (let i = 0; i < numberList.length - 2; i++) {
-    for (let j = i + 1; j < numberList.length - 1; j++) {
-      if (numberList[i] > numberList[j]) {
-        count++;
-      }
-    }
-  }
-  console.log(count);
-  return count % 2 === 0;
-};
-
-const changeNotEmptyLastTwo = () => {
-  const lastNotEmptyIndex = numberList.length - 2;
-  const lastNotEmptyElement = numberList[lastNotEmptyIndex];
-  numberList[lastNotEmptyIndex] = numberList[lastNotEmptyIndex - 1];
-  numberList[lastNotEmptyIndex - 1] = lastNotEmptyElement;
-};
-
-const fillMap = () => {
-  const objectList = [
-    { x: 0, y: 0, text: numberList[0] },
-    { x: 0, y: 1, text: numberList[1] },
-    { x: 0, y: 2, text: numberList[2] },
-    { x: 1, y: 0, text: numberList[3] },
-    { x: 1, y: 1, text: numberList[4] },
-    { x: 1, y: 2, text: numberList[5] },
-    { x: 2, y: 0, text: numberList[6] },
-    { x: 2, y: 1, text: numberList[7] },
-    { x: 2, y: 2, text: numberList[8] },
-  ];
-
-  for (let i = 0; i < objectList.length; i++) {
-    const x = objectList[i].x;
-    const y = objectList[i].y;
-    baseMap[x][y] = objectList[i];
-  }
-};
-
-function InitializeMap() {
-  shuffleNumberList();
-  fillMap();
-  return baseMap;
+for (let i = 0; i < objectList.length; i++) {
+  const x = objectList[i].x;
+  const y = objectList[i].y;
+  baseMap[x][y] = objectList[i];
 }
 
-export default InitializeMap;
+export default baseMap;
